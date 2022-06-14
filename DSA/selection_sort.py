29104ee2c073d3dc1ff38b3b6366ac1182ba9711
Md-Anisujjaman <contact.anisujjaman@gmail.com>
2022-06-14 16:48:25 +0600

    Selection Sort

--- DSA/selection_sort.py
+++ DSA/selection_sort.py
@@ -1,6 +1,17 @@
-def sort(abc):
-    for i in range(len(abc)):
-        for j in range(len(abc) - 1):
-            if abc[j] > abc[j + 1]:
-                abc[j], abc[j + 1] = abc[j + 1], abc[j]
-    return abc
+def selection_sort(values):
+    sorted_list = []
+    for i in range (0, len(values)):
+        index_to_move = index_of_min(values)
+        sorted_list.append(values.pop(index_to_move))
+    return sorted_list
+
+def index_of_min(values):
+    min_index = 0
+    for i in range(0, len(values)):
+        if values[i] < values[min_index]:
+            min_index = i
+    return min_index
+
+a = [1, 5 , 9 , 2 , 6 , 3 , 7 , 4 , 8]
+
+print(selection_sort(a))
