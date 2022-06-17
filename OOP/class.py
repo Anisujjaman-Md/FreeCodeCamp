e8cae4ec152f3a4d5fb59ebc2d131a713642d946
Md-Anisujjaman <contact.anisujjaman@gmail.com>
2022-06-18 03:05:25 +0600

    Class Updateed

--- OOP/class.py
+++ OOP/class.py
@@ -1,1 +1,23 @@
-#This is a Class
+class Item:
+    #Class Atribute
+    pay_rate = 0.8 #The Pay Rate is 0.8
+
+
+    def __init__(self, name : str, price : float,  quantity: int = 0):
+        # Run validtion on the input
+        assert price  >=0, f"Price {price} cannot be negative"
+        assert quantity >= 0, f"Quantity {quantity} cannot be negative"
+
+        #Assing to self object
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
+
+print(Item.pay_rate)
+
+l = Item("Laptop", 1000,  2)
+print(l.calculate_total_price())
