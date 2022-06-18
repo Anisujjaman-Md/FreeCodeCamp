abae84133c10eba713445c6494b71c7cb0fd25cd
Md-Anisujjaman <contact.anisujjaman@gmail.com>
2022-06-18 23:11:03 +0600

    Class Updateed V3

--- OOP/class.py
+++ OOP/class.py
@@ -48,6 +48,7 @@ item2 = Item("Watch", 100,  10)
 item2 = Item("HeadPhone", 100,  10)
 
 # for instance in Item.all:
-#     print(instance.name, instance.price, instance.quantity)
+#    print(instance.name, instance.price, instance.quantity)
 
-print(Item.all)
+# print(Item.all)
+print(item1.all)
